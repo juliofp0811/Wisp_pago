15bed9bef2f07bd0d110649dab7cf5ffcfe729b2
juliofp0811 <juliofp@hotmail.es>
2026-03-15 08:30:36 -0400

    Create Customer.js model file for customer table definition

---
+++ backend/src/models/Customer.js
@@ -0,0 +1,52 @@
+// Customer.js
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Adjust the path as necessary
+
+class Customer extends Model {}
+
+Customer.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  subscription_status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  sequelize,
+  modelName: 'Customer',
+  tableName: 'customer',
+  timestamps: false, // Set to true if you want Sequelize to manage createdAt and updatedAt
+});
+
+module.exports = Customer;
